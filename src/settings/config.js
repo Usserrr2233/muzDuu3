e5b4c0fa8d22c275d15e859dddd8ada575dbeb32
Usserrr2233 <163590768+Usserrr2233@users.noreply.github.com>
2024-04-05 09:32:12 +0000

    Update config.js

--- src/settings/config.js
+++ src/settings/config.js
@@ -4,7 +4,7 @@ module.exports = {
     token: process.env.TOKEN || "BOTTOKEN", // <==== PASTE YOU TOKEN
     prefix: process.env.PREFIX || "f?", // <==== SET YOU PRERIX BOT [ OWNER COMMANDS ]
     color: process.env.EMBED_COLOR || "#c50632", // <==== YOU EMBEDED HEX COLOR
-    owner: process.env.OWNER_ID || "900708551805268019", // <==== BOTS OWNER ID
+    owner: process.env.OWNER_ID || "", // <==== BOTS OWNER ID
     guildLogs: process.env.GUILD_LOGS || "000000000000000", // <==== YOUR SERVER JOIN LEFT LOGS CHANNEL ID
     leaveTimeout: process.env.LEAVE_TIMEOUT || "20000", // <==== SET LEAVE TIMEOUT WHEN BOT WAS ALONE || 1000 = 1sec
     disableYouTube: parseBoolean(process.env.DISABLE_YOUTUBE || "false"), // <==== SET "TRUE OR FALSE" | ENABLE/DISABLE YOUTUBE FEATURES. DISABLING THIS WILL MAKE "AUTOPLAY" COMMANDS USELESS!!!
